36b03d2cb5b244ee9ce21ab60737129ace290731
Xtian69420 <signo.303167@fairview.sti.edu.ph>
2025-04-13 00:16:37 +0800

    added reverse cage

--- routes/views.js
+++ routes/views.js
@@ -21,7 +21,6 @@ const UAParser = require('ua-parser-js');
 
 router.post('/addViews', async (req, res) => {
     try {
-
         const lastView = await viewsmodel.findOne().sort({ view: -1 });
         const nextView = lastView ? lastView.view + 1 : 1;
 
@@ -33,22 +32,29 @@ router.post('/addViews', async (req, res) => {
         const deviceOS = result.os.name || 'Unknown OS';
         const browser = result.browser.name || 'Unknown Browser';
 
-        const ipAddress = req.ip;
-        var api_key = process.env.IP_STACK;
+        let city = 'Unknown Location';
 
-        const locationResponse = await axios.get(`http://api.ipstack.com/${ipAddress}?access_key=${api_key}`);
-        const location = locationResponse.data.city || 'Unknown Location';
+        if (req.body.location) {
+            const [latitude, longitude] = req.body.location.split(',');
+
+            const geoRes = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${apiKey}`);
+            city = geoRes.data.results[0]?.components?.city ||
+                   geoRes.data.results[0]?.components?.town ||
+                   geoRes.data.results[0]?.components?.village ||
+                   geoRes.data.results[0]?.components?.state ||
+                   'Unknown Location';
+        }
 
         const newView = new viewsmodel({
             view: nextView,
             device: `${deviceOS} ${deviceType} (${browser})`,
-            location: location
+            location: city
         });
 
         const savedView = await newView.save();
         res.status(201).json(savedView);
-
     } catch (err) {
+        console.error('Error saving view:', err.message);
         res.status(500).json({ message: err.message });
     }
 });
