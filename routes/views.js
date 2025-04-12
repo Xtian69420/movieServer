3064a827927fa8edaf72794f80513bf256602f80
Xtian69420 <signo.303167@fairview.sti.edu.ph>
2025-04-13 00:20:06 +0800

    added api key

--- routes/views.js
+++ routes/views.js
@@ -36,7 +36,7 @@ router.post('/addViews', async (req, res) => {
 
         if (req.body.location) {
             const [latitude, longitude] = req.body.location.split(',');
-
+            var apikey = process.env.OPENCAGE_API_KEY;
             const geoRes = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${apiKey}`);
             city = geoRes.data.results[0]?.components?.city ||
                    geoRes.data.results[0]?.components?.town ||
