c2732d6a4b553804a66430743f7285503fa6d3f3
Xtian69420 <signo.303167@fairview.sti.edu.ph>
2025-04-13 00:06:29 +0800

    Update views.js

--- routes/views.js
+++ routes/views.js
@@ -21,11 +21,10 @@ const UAParser = require('ua-parser-js');
 
 router.post('/addViews', async (req, res) => {
     try {
-        // Get the last view number and increment it
+
         const lastView = await viewsmodel.findOne().sort({ view: -1 });
         const nextView = lastView ? lastView.view + 1 : 1;
 
-        // Parse user-agent info
         const parser = new UAParser();
         parser.setUA(req.headers['user-agent']);
         const result = parser.getResult();
@@ -34,21 +33,18 @@ router.post('/addViews', async (req, res) => {
         const deviceOS = result.os.name || 'Unknown OS';
         const browser = result.browser.name || 'Unknown Browser';
 
-        // Get the IP address from the request (assuming you're using a proxy like nginx)
         const ipAddress = req.ip;
         var api_key = process.env.IP_STACK;
-        // Use an API like ipstack to get the location (make sure to replace with your actual API key)
+
         const locationResponse = await axios.get(`http://api.ipstack.com/${ipAddress}?access_key=${api_key}`);
         const location = locationResponse.data.city || 'Unknown Location';
 
-        // Create a new view object
         const newView = new viewsmodel({
             view: nextView,
             device: `${deviceOS} ${deviceType} (${browser})`,
             location: location
         });
 
-        // Save the view to the database
         const savedView = await newView.save();
         res.status(201).json(savedView);
 
